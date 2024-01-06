d776899fa25ed9a293c98e91e2124f94ae01db53
Mohit Joshi <b210055@nitsikkim.ac.in>
2024-01-06 14:22:47 +0530

    commit

--- src/index.js
+++ src/index.js
@@ -2,27 +2,23 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const { PORT } = require('./config/serverConfig');
 
-// const { sendBasicEmail } = require('./services/email-service');
 const TicketController = require('./controllers/ticket-controller');
 const jobs = require('./utils/job');
 
-const setupAndStartServer = () => {
+const { createChannel } = require('./utils/messageQueue');
+
+const setupAndStartServer = async () => {
     const app = express();
     app.use(bodyParser.json());
     app.use(bodyParser.urlencoded({extended: true}));
     
+    // const channel = await createChannel();
+
     app.post('/api/v1/tickets',TicketController.create); 
 
     app.listen(PORT, () => {
         console.log("Server successfully started at PORT: ", PORT);
         jobs();
-        // sendBasicEmail(
-        //     '"Support" <support@admin.com>',
-        //     'b210055@nitsikkim.ac.in',
-        //     'This is a testing email',
-        //     'We hope that you will love our service'
-        // );
-
     });
 }
 
